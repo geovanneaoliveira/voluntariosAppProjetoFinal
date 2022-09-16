b275fc71e42d8a160d3f278315710f1571e725ba
Débora Labes Zavadniak <dlzavadniak@furb.br>
2022-09-16 18:42:12 -0300

    patr

--- voluntariosAppSpring/src/main/java/com/entra21/voluntariosApp/model/entity/EventoEntity.java
+++ voluntariosAppSpring/src/main/java/com/entra21/voluntariosApp/model/entity/EventoEntity.java
@@ -3,6 +3,7 @@ package com.entra21.voluntariosApp.model.entity;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 import javax.persistence.*;
 
@@ -23,9 +24,13 @@ public class EventoEntity {
     @JoinColumn(name = "id_organizadora",referencedColumnName = "id")
     private OrganizacaoEntity organizacao;
 
-    @ManyToOne
-    @JoinColumn(name = "id_patrocinador", referencedColumnName = "id")
-    private PatrocinadorEntity patrocinador;
+    @ManyToMany
+    @JoinTable(
+            name = "patrocinadores_evento",
+            joinColumns = @JoinColumn(name = "id_evento", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_patrocinador", referencedColumnName = "id")
+    )
+    private List<PatrocinadorEntity> patrocinadores;
 
     //@ManyToOne
     //@JoinColumn(name = "tags_evento", referencedColumnName = "id")
