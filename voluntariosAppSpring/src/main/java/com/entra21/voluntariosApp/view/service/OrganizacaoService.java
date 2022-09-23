cce7137190e63612e6bc204da5d381b0726e3063
geovanneaoliveira <geovanne@vortexnet.com.br>
2022-09-22 21:39:11 -0300

    JavaDoc

--- voluntariosAppSpring/src/main/java/com/entra21/voluntariosApp/view/service/OrganizacaoService.java
+++ voluntariosAppSpring/src/main/java/com/entra21/voluntariosApp/view/service/OrganizacaoService.java
@@ -23,6 +23,17 @@ public class OrganizacaoService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+
+    /**
+     * Adiciona uma Organização ao repositório.<br>
+     * Atributos de OrganizacaoDTO:
+     * <li>String nome</li>
+     * <li>String descricao</li>
+     * <li>Long idSupervisor</li>
+     * <li>String cnpj</li>
+     * @param organizacaoDTO
+     * @throws ResponseStatusException
+     */
     public void addOrganizacao(OrganizacaoDTO organizacaoDTO) {
         pessoaRepository.findById(organizacaoDTO.getIdSupervisor()).ifPresentOrElse(pessoa -> {
             OrganizacaoEntity organizacaoEntity = new OrganizacaoEntity();
@@ -33,7 +44,12 @@ public class OrganizacaoService {
         }, () -> {throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pessoa não encontrada!");});
     }
 
-    public List<OrganizacaoBuscaDTO> getOrgs(String nomeOrg) {
+    /**
+     * Busca todas as Organizações que possuem nomes similares ao passado por parâmetro.
+     * @param nomeOrg
+     * @return List {@code <OrganizacaoBuscaDTO>}
+     */
+    public List<OrganizacaoBuscaDTO> getOrganizacoes(String nomeOrg) {
         List<OrganizacaoEntity> orgs = organizacaoRepository.findAll().stream()
                 .filter(org -> org.getNome().toLowerCase().contains(nomeOrg.toLowerCase())).collect(Collectors.toList());
         return orgs.stream().map(orgE -> {
@@ -45,4 +61,61 @@ public class OrganizacaoService {
             return dto;
         }).collect(Collectors.toList());
     }
+<<<<<<< Updated upstream
+=======
+
+    /**
+     * Atualiza a Organização cujo Id for igual ao passado por parâmetro para as informações
+     * contidas em um OrganizacaoDTO.
+     * <li>String nome</li>
+     * <li>String descricao</li>
+     * <li>Long idSupervisor</li>
+     * <li>String cnpj</li>
+     * @param id
+     * @param dto
+     * @throws ResponseStatusException
+     */
+    public void updateOrganizacao(Long id, OrganizacaoDTO dto) {
+        organizacaoRepository.findById(id).ifPresentOrElse(org -> {
+            org.setNome(dto.getNome());
+            org.setDescricao(dto.getDescricao());
+            org.setSupervisor(org.getSupervisor());
+            org.setCnpj(dto.getCnpj());
+            organizacaoRepository.save(org);
+        }, () -> {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Organização não encontrada!");
+        });
+    }
+
+    /**
+     * Ativa ou desativa uma Organização de acordo com seu Status atual.
+     * @param id
+     * @throws ResponseStatusException
+     * */
+    public void status(Long id){
+        organizacaoRepository.findById(id).ifPresentOrElse(org -> {
+            org.setAtivo(!org.getAtivo());
+            organizacaoRepository.save(org);
+        } , () -> {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Organização não encontrada!");
+        });
+    }
+
+    /**
+     * Retorna uma todas as Organizações sob um mesmo supervisor de acordo com o seu Id.
+     * @param idSupervisor
+     * @return List {@code <OrganizacaoBuscaDTO>}
+     */
+    public List<OrganizacaoBuscaDTO> buscarOrgsPorSurpervisor (Long idSupervisor){
+        return organizacaoRepository.findAllBysupervisor_Id(idSupervisor).stream().map(orgs -> {
+            OrganizacaoBuscaDTO dto = new OrganizacaoBuscaDTO();
+            dto.setNomeOrg(orgs.getNome());
+            dto.setDescricao(orgs.getDescricao());
+            dto.setNomeSupervisor(orgs.getSupervisor().getNome());
+            dto.setSobrenomeSupervisor(orgs.getSupervisor().getSobrenome());
+            return dto;
+        }).collect(Collectors.toList());
+
+    }
+>>>>>>> Stashed changes
 }
--- voluntariosAppSpring/src/main/java/com/entra21/voluntariosApp/view/service/TagsService.java
+++ voluntariosAppSpring/src/main/java/com/entra21/voluntariosApp/view/service/TagsService.java
@@ -27,12 +27,28 @@ public class TagsService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    /**Adiciona uma Tag com o nome passado por parâmetro ao repositório.
+     * @param nomeTag
+     * @throws ResponseStatusException
+     * */
     public void addTag(String nomeTag) {
+        tagsRepository.findAll().forEach(tag -> {
+            if (tag.getNome().equalsIgnoreCase(nomeTag)){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tag já existente!");
+            }
+        });
         TagsEntity tagsEntity = new TagsEntity();
         tagsEntity.setNome(nomeTag);
         tagsRepository.save(tagsEntity);
     }
 
+    /**Adiciona Tags à um Evento.<br>
+     * Atributos de TagsEventoDTO
+     * <li>Long idEvento</li>
+     * <li>List {@code <Long>} idTagsEvento</li>
+     * @param tagsEventoDTO
+     * @throws ResponseStatusException
+     * */
     public void setTagsEvento(TagsEventoDTO tagsEventoDTO) {
         eventoRepository.findById(tagsEventoDTO.getIdEvento()).ifPresentOrElse(ev -> {
             List<TagsEntity> tagsEntities = new ArrayList<>(tagsRepository.findAllById(tagsEventoDTO.getIdTagsEvento()));
@@ -41,6 +57,13 @@ public class TagsService {
         }, () -> {throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Evento não encontrado!");});
     }
 
+    /**Adiciona Tags ao Usuário.<br>
+     * Atributos de TagsPessoaDTO
+     * <li>Long idPessoa</li>
+     * <li>List {@code <Long>} idTagsPessoa</li>
+     * @param tagsPessoaDTO
+     * @throws ResponseStatusException
+     * */
     public void setTagsPessoa(TagsPessoaDTO tagsPessoaDTO) {
         pessoaRepository.findById(tagsPessoaDTO.getIdPessoa()).ifPresentOrElse(user -> {
             List<TagsEntity> tagsEntities = new ArrayList<>(tagsRepository.findAllById(tagsPessoaDTO.getIdTagsPessoa()));
@@ -49,12 +72,20 @@ public class TagsService {
         }, () -> {throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário não encontrado!");});
     }
 
+    /**Remove uma Tag do repositório através do seu Id.
+     * @param idTag
+     * */
     public void deleteTag(Long idTag) {
         eventoRepository.findAll().forEach(ev -> ev.getTags().removeIf(tag -> Objects.equals(tag.getId(), idTag)));
         pessoaRepository.findAll().forEach(pe -> pe.getTags().removeIf(tag -> Objects.equals(tag.getId(), idTag)));
         tagsRepository.deleteById(idTag);
     }
 
+    /**Atualiza a Tag cujo Id for igual ao passado por parâmetro com a String novoNome.
+     * @param idTag
+     * @param novoNome
+     * @throws ResponseStatusException
+     * */
     public void atualizarTag(Long idTag, String novoNome) {
         tagsRepository.findById(idTag).ifPresentOrElse(tag -> {
             tag.setNome(novoNome);
